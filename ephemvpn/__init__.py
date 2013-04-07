a6537f67a511eb3e657954628691d9de5471f077
Casey Link <unnamedrambler@gmail.com>
2013-04-07 16:08:28 +0200

    funify the output text

--- ephemvpn/__init__.py
+++ ephemvpn/__init__.py
@@ -3,8 +3,10 @@ from fabric.api import settings
 from ephemvpn import configuration as config
 from ephemvpn import vpntypes
 from ephemvpn import awshelpers as aws
-
 import logging as log
+
+__version_info__ = (0,1)
+__version__ = '.'.join(map(str, __version_info__))
 
 
 def launch(vpn, aws_region):
@@ -17,8 +19,8 @@ def launch(vpn, aws_region):
     root_device = '/dev/sda1'
     root_device_size = '10'
 
-    log.info('authenticating to EC2')
     ec2_connection= aws._get_ec2_connection(aws_region)
+    log.info('preparing cloud conjuring ritual')
     aws._create_security_group(ec2_connection)
     ami_id = config.AMI_ID_BY_REGION[ec2_connection.region.name]
     mapping = aws._get_block_device_mapping(root_device, root_device_size, delete_on_terminate=True)
@@ -34,17 +36,18 @@ def launch(vpn, aws_region):
 
     # TODO good idea to assume it's the first one?
     instance = reservation.instances[0]
-    log.info('spinning up the instance')
+    log.info('summoning a vpn from the cloud . . .')
     sleep(10)
 
     instance.update()
 
     while instance.state != 'running':
-        sleep(10)
+        sleep(3)
+        log.info(' . . .')
         instance.update()
 
     if instance.state == 'running':
-        log.info('new instance "' + instance.id + '" accessible at ' + instance.public_dns_name)
+        log.debug('new instance "' + instance.id + '" accessible at ' + instance.public_dns_name)
     else:
         log.error('starting failed. instance status: ' + instance.state)
         return None
@@ -54,7 +57,7 @@ def launch(vpn, aws_region):
 
 
 def configure(hostname, vpn):
-    log.info('configuring instance {0}'.format(hostname))
+    log.debug('configuring instance')
     with settings(host_string=hostname, key_filename=config.LOCAL_AWS_KEY_FILE,
         user=config.AWS_USER_NAME,connection_attempts=10):
         vpn.post_configure()
